01a0cf3c3f495323ed58a2fa3966f00b6015625d
luzbrendaoliv <127051688+luzbrendaoliv@users.noreply.github.com>
2023-06-24 18:10:39 -0300

    New Trigger

--- Scripts/Triggers.sql
+++ Scripts/Triggers.sql
@@ -11,3 +11,24 @@ $function$
 ;
 
 create trigger customerorder_bi before insert on customerorder for each row execute procedure f_customerorder_bi();
+
+---------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION f_orderitem_aiu()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $function$
+declare item_price numeric;
+declare total_order numeric;
+begin
+	select price into item_price from item where itemid = new.itemid; 
+	new.total_price := item_price * new.quantity;
+
+	select SUM(total_price) into total_order from orderitem where orderid = new.orderid;
+	update customerorder set total = total_order where orderid = new.orderid;
+
+	RETURN NEW;
+END;
+$function$;
+
+create trigger orderitem_aiu after INSERT OR UPDATE on orderitem for each row execute function f_orderitem_aiu();
